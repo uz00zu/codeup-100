d893a1b5b6b182b70a0dde24080a9ce159406db0
uz00zu <>
2021-05-07 05:49:31 +0000

    1065번

---
+++ 기초 1 출력문/1065.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	if (a % 2 == 0) {
+		printf("%d\n", a);
+	}
+	else { ; }
+	if (b % 2 == 0) {
+		printf("%d\n", b);
+	}
+	else { ; }
+	if (c % 2 == 0) {
+		printf("%d\n", c);
+	}
+	else { ; }
+	return 0;
+}
